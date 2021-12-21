0b7d37dabe5e5ea00ac1f8babe5291d9730bc6fd
bssgowtham <55312214+bssgowtham@users.noreply.github.com>
2021-12-21 10:28:25 +0530

    created the application skeleton using express-generator and created category model

---
+++ models/category.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Import schema from mongoose
+const Schema = mongoose.Schema;
+
+// Create schema
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 30
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 256
+    }
+});
+
+// Virtual for URL
+categorySchema.virtual('url')
+    .get(function() {
+        return '/inventory/category' + this._id;
+    })
+
+
+// Export model
+module.exports = mongoose.model('Category', categorySchema);
